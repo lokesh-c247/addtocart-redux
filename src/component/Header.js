255eb7ade67e6bed075813725ca48b77ffc21445
lokesh-c247 <lokesh.g@chapter247.com>
2023-08-25 18:47:12 +0530

    i18n added

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,18 +1,38 @@
+import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart} from "../redux/cartSlice";
+import i18next from '../i18n';
+import React from "react";
+import i18n from "i18next";
+
+
 const Header = () => {
+    const {t} = useTranslation();
 
     const allItems = useSelector((state) => state.cart.items)
     const dispatch = useDispatch();
+    const [locale , setLocal] = React.useState(i18n.language);
+
+    i18next.on("languageChanged" , (lng) => setLocal(i18n.language))
+
+    const handleChange = (event) => {
+        i18next.changeLanguage(event.target.value);
+    }
     
     return (
        <div style={{display : "flex" , alignItems : "center" , justifyContent : "space-around" , margin : "1rem" }}>
-            <div><h1>Food App</h1></div>
+            <div><h1>{t("title")}</h1></div>
             <div>
                 <ul style={{listStyle : "none" , display : "flex" , flexDirection : "column" , gap : "5px"}}>
                     <li>Quantity :- {allItems.length}</li>
                     <button ><li data-testId = "clearCartBtn" onClick={()=>{dispatch(clearCart())}} >Clear cart</li></button>
                 </ul>
+            </div>
+            <div>
+                <select value={locale} onChange={handleChange}>
+                    <option value="en">English</option>
+                    <option value="hi">Hindi</option>
+                </select>
             </div>
        </div>
     )
--- src/component/Main.js
+++ src/component/Main.js
@@ -10,7 +10,7 @@ const Main = () => {
 
     useEffect(() => {
         dispatch(allData());
-    }, [])
+    }, []);
 
     console.log(allItems , "allitems");
 
